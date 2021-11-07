bf5e4465fd036d8a8291aa819b52472ae87f4dd3
rednithin <reddy.nithinpg@live.com>
2021-11-07 15:53:58 +0530

    Social Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+// admin/pages/custom-page.tsx
+import { useGoogleLogin } from "react-google-login";
+import FacebookLogin from 'react-facebook-login';
 
-function App() {
+export default function App() {
+  const { signIn, loaded } = useGoogleLogin({
+    clientId:
+      "690539792704-4bjr1g1qvlm0t7oakrvacqr7mg5vp0qa.apps.googleusercontent.com",
+    scope:
+      "",
+    onSuccess: (response) => {
+      console.log("Success", { response, code: response.code });
+    },
+    onFailure: (response) => {
+      console.log("Failure", { response });
+    },
+    responseType: "code",
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {
+        loaded && (
+
+          <button onClick={signIn}>Google sign in</button>
+        )
+      }
+      <FacebookLogin
+        appId="416373366689797"
+        autoLoad={false}
+        fields="name,email,picture"
+        scope="public_profile,email"
+        callback={(response) => {
+          const code = JSON.parse(atob(response.signedRequest.split(".")[1])).code;
+          console.log({response, code})
+        }}
+        render={renderProps => (
+          <button onClick={renderProps.onClick}>This is my custom FB button</button>
+        )}
+        responseType="code"
+      />
     </div>
   );
 }
-
-export default App;
