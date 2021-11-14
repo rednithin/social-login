6b6b9fedd1d0c1a4ab561f2e709136743b331cc1
rednithin <reddy.nithinpg@live.com>
2021-11-14 15:20:05 +0530

    Updated flow

--- src/App.js
+++ src/App.js
@@ -27,7 +27,7 @@ export default function App() {
         scope="public_profile,email"
         callback={(response) => {
           const code = JSON.parse(atob(response.signedRequest.split(".")[1])).code;
-          console.log({response, code})
+          console.log({response, access_token: response.accessToken})
         }}
         render={renderProps => (
           <button disabled={renderProps.isDisabled || renderProps.isProcessing || !renderProps.isSdkLoaded} className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10 disabled:bg-gray-400" onClick={renderProps.onClick}>Facebook Login</button>
